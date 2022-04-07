f0056815f9b303be7d4ff95b29060760d03c1c35
Provashish Roy <provashishroy95@gmail.com>
2022-04-07 18:48:16 +0600

    added react bootstrap

--- src/App.js
+++ src/App.js
@@ -5,10 +5,26 @@ import app from './firebase.init';
 const auth = getAuth(app);
 
 function App() {
+  const handleEmailBlur = (event) => {
+    console.log(event.target.value);
+  }
+  const handlePasswordBlur = (event) => {
+    console.log(event.target.value);
+  }
+  const hanldeFormSubmit = (event) => {
+    console.log('form submited');
+    event.preventDefault();
+  }
   return (
     <div className="App">
-      <form action="">
-
+      <form onSubmit={hanldeFormSubmit}>
+        Name:
+        <input type="text" name="" id="" /><br />
+        Password:
+        <input onBlur={handlePasswordBlur} type="password" name="" id="" /><br />
+        Email:
+        <input onBlur={handleEmailBlur} type="email" name="" id="" /><br />
+        <input type="submit" value="Login" /><br />
       </form>
     </div>
   );
