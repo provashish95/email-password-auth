fd464a1be8bc14b5a6c64479b0e6c57fd5008520
Provashish Roy <provashishroy95@gmail.com>
2022-04-07 19:23:26 +0600

    added user  on firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 
-import { getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import app from './firebase.init';
 import Form from 'react-bootstrap/Form'
 import { Button } from 'react-bootstrap';
+import { useState } from "react";
 
 const auth = getAuth(app);
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const handleEmailBlur = (event) => {
-    console.log(event.target.value);
+    setEmail(event.target.value);
   }
   const handlePasswordBlur = (event) => {
-    console.log(event.target.value);
+    setPassword(event.target.value);
   }
   const handleFormSubmit = (event) => {
-    console.log('form submitted');
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+    // console.log('form submitted', email, password);
     event.preventDefault();
   }
+
   return (
     <div >
       <div className="registration w-50 mx-auto mt-5">
